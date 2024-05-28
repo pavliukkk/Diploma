1d718ef10e8e578dae73c0632f5303756dae0e96
pavliukkk <126521423+pavliukkk@users.noreply.github.com>
2024-05-28 23:12:39 +0300

    Update views.py

--- kyrsova/views.py
+++ kyrsova/views.py
@@ -559,14 +559,6 @@ def profile_info(request, username):
     user = get_object_or_404(User, username=username)
 
     reservations = Reservation_main.objects.filter(user=user)
-
-    reservations_with_meals = []
-    for reservation in reservations:
-        meals = ReservationMeal.objects.filter(reservation=reservation)
-        reservations_with_meals.append({
-            'reservation': reservation,
-            'meals': meals
-        })
     
     try:
         user_profile = UserProfile.objects.get(user=user)
@@ -578,7 +570,7 @@ def profile_info(request, username):
         # Якщо ні, перенаправте користувача на власний профіль
         return redirect('profile', username=request.user.username)
 
-    return render(request, 'profile.html', {'reservations_with_meals': reservations_with_meals, 'user_profile': user_profile})
+    return render(request, 'profile.html', {'reservations': reservations, 'user_profile': user_profile})
 
 def crop_to_circle(image_path):
     # Відкриття завантаженої фотографії за допомогою Pillow
