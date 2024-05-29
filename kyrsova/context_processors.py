744d8134f0bdc9e9f2ed42dbfd94d0c6de051430
pavliukkk <126521423+pavliukkk@users.noreply.github.com>
2024-05-29 22:58:57 +0300

    Update context_processors.py

--- kyrsova/context_processors.py
+++ kyrsova/context_processors.py
@@ -1,8 +1,10 @@
-from .models import UserProfile
+from .models import Reservation_main, UserProfile
 
 def add_user_profile_to_context(request):
+    user=request.user
     user_profile = None
+    reservations = Reservation_main.objects.filter(user=user)
     if request.user.is_authenticated:
         user_profile = UserProfile.objects.get(user=request.user)
 
-    return {'user_profile': user_profile}
+    return {'reservations': reservations, 'user_profile': user_profile}
